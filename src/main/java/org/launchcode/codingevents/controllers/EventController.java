dd3cb6ba81f66038468e1ba2beb6fec81d2ec250
emmaran <emmalrandolph@gmail.com>
2022-10-04 20:03:10 -0500

    Commit to 'my-views-exercises-solution'

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -8,24 +8,36 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 @Controller
 @RequestMapping("events")
 public class EventController {
 
-    private static List<String> events = new ArrayList<>();
+//    private static List<String> events = new ArrayList<>();
+    private static HashMap<String, String> events = new HashMap<>();
 
     @GetMapping
     public String displayAllEvents(Model model) {
+        // first attempt
 //        ArrayList<String> events = new ArrayList<>();
 //        events.add("Code With Pride");
 //        events.add("Strange Loop");
 //        events.add("Apple WWDC");
 //        events.add("SpringOne Platform");
 //        model.addAttribute("events", events);
+        // second attempt
+//        model.addAttribute("events", events);
+//        return "events/index";
+        //third attempt
+//        HashMap<String, String> events = new HashMap<>();
+        events.put("Menteaship","A fun meetup for connecting with mentors");
+        events.put("Code With Pride","A fun meetup sponsored by LaunchCode");
+        events.put("Javascripty", "An imaginary meetup for Javascript developers");
         model.addAttribute("events", events);
         return "events/index";
+
     }
 
     //lives at /events/create
@@ -35,10 +47,10 @@ public class EventController {
     }
 
     //lives at /events/create
-    @PostMapping("create")
-    public String createEvent(@RequestParam String eventName) {
-        events.add(eventName);
-        return "redirect:";
-    }
+//    @PostMapping("create")
+//    public String createEvent(@RequestParam String eventName) {
+//        events.add(eventName);
+//        return "redirect:";
+//    }
 
 }
