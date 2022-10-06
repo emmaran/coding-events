8bee13a6d5c91cc5cebb831417e230c0bb93b05d
emmaran <emmalrandolph@gmail.com>
2022-10-05 20:04:05 -0500

    Edit events functionality

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -52,6 +52,7 @@ public class EventController {
         return "redirect:";
     }
 
+    //lives at events/delete
     @GetMapping("delete")
     public String renderDeleteEventForm(Model model) {
         model.addAttribute("title", "Delete Event");
@@ -67,6 +68,25 @@ public class EventController {
                 EventData.remove(id);
             }
         }
+
+        return "redirect:";
+    }
+
+    @GetMapping("edit/{eventId}")
+    public String displayEditForm(Model model, @PathVariable int eventId) {
+        Event eventToEdit = EventData.getById(eventId);
+        model.addAttribute("event", eventToEdit);
+        String title = "Edit Event " + eventToEdit.getName() + " (id=" + eventToEdit.getId() + ")";
+        model.addAttribute("title", title);
+
+        return "events/edit";
+    }
+
+    @PostMapping("edit")
+    public String processEditForm(int eventId, String name, String description) {
+        Event eventToEdit = EventData.getById(eventId);
+        eventToEdit.setName(name);
+        eventToEdit.setDescription(description);
 
         return "redirect:";
     }
